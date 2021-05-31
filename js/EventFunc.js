7faf85a1658f5d08a1ab2f40ac9962b5914b5ae9
DevAbyss <61103879+DevAbyss@users.noreply.github.com>
2021-06-01 00:12:34 +0900

    modified nav css

--- js/EventFunc.js
+++ js/EventFunc.js
@@ -2,10 +2,12 @@
 
 const navBar = document.querySelector(".nav");
 
-if (window.scrollY > 200) {
-  console.log("add 실행");
-  navBar.classList.add("sticky");
-} else {
-  console.log("remove 실행");
-  navBar.classList.remove("sticky");
-}
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset > 80) {
+    console.log("add 실행");
+    navBar.classList.add("sticky");
+  } else {
+    console.log("remove 실행");
+    navBar.classList.remove("sticky");
+  }
+});
